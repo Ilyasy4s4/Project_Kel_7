be14d83b2ae02ac5e50028c2ea287e77d5512dfb
ilyas <ilyasgumalar@gmail.com>
2025-12-15 17:10:19 +0700

    Update fitur aplikasi kasir dan penambahan menu

---
+++ src/org/netbeans/lib/awtextra/AbsoluteLayout.java
@@ -0,0 +1,11 @@
+package org.netbeans.lib.awtextra;
+
+public class AbsoluteLayout {
+
+    public AbsoluteLayout() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+   
+    
+}
